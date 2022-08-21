77a78ea58b3ba279c333940e9e340104dd6cd345
SergeyAvg <Sery.mamonov@gmail.com>
2022-08-21 18:29:51 +0500

    more less

---
+++ homework 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Обьявляем переменные
+double num1, num2; // для преоброзавания
+string input1, input2; // для ввода
+input1 =  Console.ReadLine();
+input2 = Console.ReadLine();
+
+num1 = double.Parse(input1);
+num2 = double.Parse(input2);
+
+if (num1 > num2) // Сравниваем
+{
+    Console.Write(num1 + " больше чем " + num2);
+}
+else
+{
+    Console.Write(num2 + " Больше чем " + num1);
+}
+
